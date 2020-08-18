7037800cbfcdabceeb57aff791d85edda1e83d76
Dorly Neto <hello@dorlyneto.com>
2020-08-17 22:59:12 -0300

    Update script.js

--- 90sites/07-desconecte-se/script.js
+++ 90sites/07-desconecte-se/script.js
@@ -1,6 +1,8 @@
 let isOffline = false;
 window.addEventListener('load', checkConnectivity);
 
+document.getElementById("gif").style.display = "none"; 
+
 function checkConnectivity() {
   updateStatus();
   window.addEventListener('online', updateStatus);
@@ -13,10 +15,8 @@ function updateStatus() {
     console.log(isOffline);
     const offline_text = document.querySelector(".text");
     if (isOffline) {
-        document.getElementById("gif").style.display = "none"; 
         offline_text.innerHTML = "<div><p><h1>Chegou a hora de prestar atenção na sua respiração!</h1></p>Aproveita que você desplugou da internet por um minuto, repita esse exercício cinco vezes:</p><p><center><img src=\"assets/respire.gif\"></img></center></p><p>Conseguiu? Ótimo! Parar um pouco e prestar atenção na própria respiração é uma ótima forma de se reconectar com o presente.</p><p>Agora, aproveite que a internet ainda está desligada e se levante para alongar o corpo. Depois volte a fazer o que estava fazendo, pois eu sei que está arrasando :D</p></div>";
     } else {
-        document.getElementById("gif").style.display = "none";
         offline_text.innerHTML = "<h1>O conteúdo só vai aparecer se você ficar offline.</h1><br><br><h2>Desligue o Wi-Fi ou coloque no modo avião.<br></h2>";
     } 
    
