0c7ecdc1dd38389b741c5afaca21aefaae25ffc5
Dorly Neto <hello@dorlyneto.com>
2020-08-19 19:32:53 -0300

    upload site 09

--- 90sites/09-bateria/script.js
+++ 90sites/09-bateria/script.js
@@ -16,4 +16,3 @@ function removeTransition(e) {
   const keys = Array.from(document.querySelectorAll('.key'));
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
   window.addEventListener('keydown', playSound);
-  window.addEventListener('touchstart', playSound);
