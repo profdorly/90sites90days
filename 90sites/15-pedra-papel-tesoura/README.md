ccca0f3e20950b45a7af594e843cd84f8533a10f
Dorly Neto <hello@dorlyneto.com>
2020-08-25 23:14:12 -0300

    upload site 15

---
+++ 90sites/15-pedra-papel-tesoura/README.md
@@ -0,0 +1,37 @@
+##### Desafio: desenvolvendo um site por dia por 90 dias 
+
+# [Site #15 - Pedra, papel ou tesoura?](https://www.dorlyneto.com/90sites/15-pedra-papel-tesoura)
+
+## Pedra, papel ou tesooooooooura!
+
+Ainda não conheci uma pessoa que nunca tenha brincado de pedra, papel ou tesoura (ou Jokenpô, dependendo de sua região). Uma brincadeira milenar e super divertida. Por que não adaptá-la para o mundo virtual, e usar a lógica da programação para disputar com a máquina?
+
+Foi o que fiz neste site, que você pode conferir a seguir:
+
+* [Site #15 - Pedra, papel ou tesoura?](https://www.dorlyneto.com/90sites/15-pedra-papel-tesoura)
+
+## O que aprendi na construção deste site?
+
+Conversando com a dev [Nadia Ligia](https://github.com/nlnadialigia), ela me contou que tem desenvolvido jogos para aprimorar o seu conhecimento em programação. Na hora me inspirei a querer fazer um também. Fui na internet buscar algum tutorial que me auxiliasse neste missão, e encontrei este aqui, criado pelo professor Felipe Fontoura:
+
+[![Tutorial do professor Felipe Fontoura](https://img.youtube.com/vi/JP3eYU137t8/hqdefault.jpg)](https://www.youtube.com/watch?v=JP3eYU137t8)
+
+De cara, gostei da didática do professor, que fez questão de nomear alguns elementos do Javascript em português. Penso muito nas pessoas que ainda não possuem fluência em inglês e estão se aventurando por este mundo doido da programação. Tutoriais assim ajudam a incluir mais gente na comunidade.
+
+### Math.random() é poderoso demais
+
+A função [Math.random()](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random) é a base fundamental da lógica de programação deste site. Sem ela, não é possível determinar aleatoriamente qual opção o computador irá escolher a cada rodada do jogo. Eu já tinha a usado no site [08 - tente clicar](https://www.dorlyneto.com/90sites/08-tente-clicar/), mas com outra finalidade. Ao usá-la agora para este projeto, entendi seu potencial para o desenvolvimento de jogos simples.
+
+### Else if if else else if...
+
+O código computacional, por utilizar uma base matemática binária, é universal. Em qualquer canto do mundo, 0 é 0 e 1 é 1. Isso é inquestionável (será?). Dessa forma, qualquer jogo que represente uma lógica na escolha de um vencedor pode ser desenvolvido para ser disputado contra uma máquina. Hoje tive a felicidade de aprender a desenvolver um jogo simples. Porém, imagino o trabalho danado que desenvolvedores possuem para programar jogos complexos, ou máquinas que utilizam inteligência artificial para disputar contra seres humanos.
+
+E máquinas contra máquinas, é possível? Sim! Eu amo este exemplo aqui, de dois robôs disputando uma partida de xadrez. O que tem de monótono tem de impressionante:
+
+[![Dois robôs jogando xadrez](https://img.youtube.com/vi/65YDAXfSAWw/hqdefault.jpg)](https://www.youtube.com/watch?v=65YDAXfSAWw)
+
+---
+
+Até a próxima e rumo ao próximo nível! 🚀🚀🚀
+
+
---
+++ 90sites/15-pedra-papel-tesoura/script.js
@@ -0,0 +1,116 @@
+var jogadorNome;
+    var jogadorPontos = 0;
+    var jogadorEscolha = 0;
+
+    var computadorEscolha = 0;
+    var computadorPontos = 0;
+
+    function sortear(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function traduzirEscolha(numero) {
+      if(numero == 1) {
+        return 'Pedra';
+      }
+      else if(numero == 2) {
+        return 'Papel';
+      }
+      else if(numero == 3) {
+        return 'Tesoura';
+      }
+    }
+
+    function selecionar(tipo, escolha) {
+      document.getElementById(tipo + '-escolha-' + escolha).classList.add('selecionado');
+    }
+
+    function deselecionar(tipo, escolha) {
+      document.getElementById(tipo + '-escolha-' + escolha).classList.remove('selecionado');
+    }
+
+    function mensagem(texto){
+      document.getElementById('mensagem').innerHTML = texto;
+    }
+
+    function definirJogadorNome(nome) {
+      document.getElementById('jogador-nome').innerHTML = nome;
+    }
+
+ 
+    function calcularEscolha(jogador, computador) {
+      if (jogador == 1 && computador == 1) {
+        return 0;
+      }
+      else if (jogador == 1 && computador == 2) {
+        return 2;
+      }
+      else if (jogador == 1 && computador == 3) {
+        return 1;
+      }
+      else if (jogador == 2 && computador == 1) {
+        return 1;
+      }
+      else if (jogador == 2 && computador == 2) {
+        return 0;
+      }
+      else if (jogador == 2 && computador == 3) {
+        return 2;
+      }
+      else if (jogador == 3 && computador == 1) {
+        return 2;
+      }
+      else if (jogador == 3 && computador == 2) {
+        return 1;
+      }
+      else if (jogador == 3 && computador == 3) {
+        return 0;
+      }
+    }
+
+    function somarPontoJogador() {
+      jogadorPontos++;
+      document.getElementById('jogador-pontos').innerHTML = jogadorPontos;
+    }
+
+    function somarPontoComputador() {
+      computadorPontos++;
+      document.getElementById('computador-pontos').innerHTML = computadorPontos;
+    }
+
+    function jogar(escolha) {
+      jogadorEscolha = escolha;
+      selecionar('jogador', jogadorEscolha);
+
+      computadorEscolha = sortear(1, 3);
+      selecionar('computador', computadorEscolha);
+
+      var ganhador = calcularEscolha(jogadorEscolha, computadorEscolha);
+
+      if (ganhador == 0) {
+        mensagem('Empate');
+      }
+      else if (ganhador == 1) {
+        mensagem('Ponto para ' + jogadorNome);
+        somarPontoJogador();
+      }
+      else if (ganhador == 2) {
+        mensagem('Ponto para o Computador');
+        somarPontoComputador();
+      }
+
+      setTimeout(function() {
+        deselecionar('jogador', jogadorEscolha);
+        deselecionar('computador', computadorEscolha);
+        mensagem(jogadorNome + ' escolha uma opção...');
+      }, 3500);
+    }
+
+    document.getElementById('jogador-escolha-1').onclick = function(){ jogar(1); };
+    document.getElementById('jogador-escolha-2').onclick = function(){ jogar(2); };
+    document.getElementById('jogador-escolha-3').onclick = function(){ jogar(3); };
+
+    jogadorNome = prompt("Qual é o seu nome?");
+    definirJogadorNome(jogadorNome);
+
+    mensagem('O jogo vai começar, ' + jogadorNome + '! Pedra, papel ou tesoura? Escolha acima.');
