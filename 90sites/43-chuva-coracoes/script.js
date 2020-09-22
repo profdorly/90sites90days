03c8cdaf946959b150a091f98753d99151a43d1d
Dorly Neto <hello@dorlyneto.com>
2020-09-22 18:22:33 -0300

    upload site 43

---
+++ 90sites/43-chuva-coracoes/script.js
@@ -0,0 +1,33 @@
+// function createHeart() {
+//     const heart = document.createElement("div");
+//     heart.classList.add("heart");
+
+//     heart.style.left = Math.random() * 100 + "vw";
+//     heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+//     heart.innerText = "💜";
+
+//     document.body.appendChild(heart);
+
+//     setTimeout(() => {
+//         heart.remove();
+//     }, 5000);
+// }
+
+document.getElementById("button").onclick = function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+    heart.innerText = "💜";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(createHeart, 300);
